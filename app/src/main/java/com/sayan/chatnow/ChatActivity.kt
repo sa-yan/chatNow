d505a07b5b49d016c0bb7f1461ca03ec79287b4d
Sayan Mondal <sayan.mondal4557@gmail.com>
2024-02-13 01:40:29 +0530

    2nd commit

--- app/src/main/java/com/sayan/chatnow/ChatActivity.kt
+++ app/src/main/java/com/sayan/chatnow/ChatActivity.kt
@@ -1,11 +1,91 @@
 package com.sayan.chatnow
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import com.sayan.chatnow.databinding.ActivityChatBinding
 
 class ChatActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityChatBinding
+    private lateinit var messageList:ArrayList<MessageModel>
+    private lateinit var messageAdapter:MessageAdapter
+    private lateinit var dbRef:DatabaseReference
+
+    var receiverRoom:String? =  null
+     var senderRoom:String?=null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_chat)
+        binding = ActivityChatBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        val name = intent.getStringExtra("name")
+        val receiverUid = intent.getStringExtra("id")
+        val senderUid = FirebaseAuth.getInstance().currentUser?.uid
+        dbRef = FirebaseDatabase.getInstance().getReference()
+
+        messageList= ArrayList()
+        messageAdapter= MessageAdapter(this, messageList)
+
+        binding.chatRecycler.layoutManager=LinearLayoutManager(this)
+        binding.chatRecycler.adapter=messageAdapter
+
+        senderRoom = receiverUid+senderUid
+        receiverRoom=senderUid+receiverUid
+
+        supportActionBar?.title=name
+
+
+
+        dbRef.child("chats")
+            .child(senderRoom!!)
+            .child("messages")
+            .addValueEventListener(object:ValueEventListener{
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    messageList.clear()
+                    for(postSnapShot in snapshot.children){
+                        val message = postSnapShot.getValue(MessageModel::class.java)
+                        messageList.add(message!!)
+                    }
+
+                    messageAdapter.notifyDataSetChanged()
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+
+                }
+
+            })
+
+        Log.d("MESSAGE",messageList.toString())
+
+
+        binding.sendBtn.setOnClickListener {
+            val message = binding.messageEt.text.toString()
+            val messageObject = MessageModel(message, senderUid!!)
+
+            dbRef.child("chats").child(senderRoom!!).child("messages").push()
+                .setValue(messageObject).addOnSuccessListener {
+                    dbRef.child("chats").child(receiverRoom!!)
+                        .child("messages")
+                        .push()
+                        .setValue(messageObject)
+                }
+            binding.messageEt.setText("")
+
+        }
     }
+
+    //function to add messages in recyclerview
+
+
 }
--- app/src/main/java/com/sayan/chatnow/UserAdapter.kt
+++ app/src/main/java/com/sayan/chatnow/UserAdapter.kt
@@ -1,11 +1,14 @@
 package com.sayan.chatnow
 
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.RelativeLayout
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.auth.FirebaseAuth
 
 class UserAdapter(val context:Context, val userList:ArrayList<UserModel>): RecyclerView.Adapter<UserAdapter.UserViewHolder>() {
 
@@ -22,10 +25,18 @@ class UserAdapter(val context:Context, val userList:ArrayList<UserModel>): Recyc
         val currUser = userList[position]
         holder.name.text = currUser.name
         holder.pp.text = currUser.name?.substring(0,1)?.toUpperCase()
+
+        holder.userItem.setOnClickListener {
+            val intent = Intent(context, ChatActivity::class.java)
+            intent.putExtra("name",currUser.name)
+            intent.putExtra("id",currUser.uid)
+            context.startActivity(intent)
+        }
     }
 
     class UserViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val name = itemView.findViewById<TextView>(R.id.name_user)
         val pp = itemView.findViewById<TextView>(R.id.profileText)
+        val userItem = itemView.findViewById<RelativeLayout>(R.id.item_person)
     }
 }
